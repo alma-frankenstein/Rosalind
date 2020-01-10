af48ceef0bc4e4efa80fa87876d1e0b4b5344d4d
Alma Frankenstein <48488886+alma-frankenstein@users.noreply.github.com>
2020-01-10 13:23:09 -0800

    delete comments

--- FIB.py
+++ FIB.py
@@ -1,7 +1,12 @@
-"""Given: Positive integers n≤40 and k≤5
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Rabbits and recurrence relations
+Given: Positive integers n≤40 and k≤5
 Return: The total number of rabbit pairs that will be present after n
 months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter 
-of k rabbit pairs (instead of only 1 pair)."""
+of k rabbit pairs (instead of only 1 pair).
+"""
 
 #start with 1 pair of rabbits in each first and second months (given)
 x = [1,1]
@@ -12,6 +17,5 @@ def wascally(months, pairs_per_month):
         new_val = ((x[-2]*pairs_per_month)+x[-1])
         x.append(new_val)
         counter += 1
-    print(wascally[-1])
+    return wascally[-1]
     
-wascally(29,4) #returns 170361678269
