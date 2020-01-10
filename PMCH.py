6ef03149d92acc8ff6557219b63618cfe576e65b
Alma Frankenstein <48488886+alma-frankenstein@users.noreply.github.com>
2020-01-10 13:39:29 -0800

    add given and return statements

--- PMCH.py
+++ PMCH.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Perfect matchings and RNA secondary structures
+Given: An RNA string s having the same number of occurrences of 'A' as 'U' and the same number of occurrences of 'C' as 'G'.
+Return: The total possible number of perfect matchings of basepair edges in the bonding graph of s
+"""
+.
 
 from math import factorial
 
